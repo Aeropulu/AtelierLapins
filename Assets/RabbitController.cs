f17ce0b3374cc64406bf10f7c4767cb65c35652f
Aeropulu <blasteguaine@gmail.com>
2020-10-19 16:20:39 +0200

    return to urp

--- Assets/RabbitController.cs
+++ Assets/RabbitController.cs
@@ -6,18 +6,20 @@ using UnityEngine.AI;
 public class RabbitController : MonoBehaviour
 {
     public GameObject target;
+    private Vector3 velocity;
     // Start is called before the first frame update
     void Start()
     {
+        velocity = transform.forward;
         if (target)
         {
-            GetComponent<NavMeshAgent>().destination = target.transform.position;
+            //GetComponent<NavMeshAgent>().destination = target.transform.position;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().Move(velocity * Time.deltaTime);
     }
 }
