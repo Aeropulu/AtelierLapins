cc709c86b813ed6bab727241d1264288ee63f350
Aeropulu <blasteguaine@gmail.com>
2020-10-19 13:46:11 +0200

    Kenney Assets

---
+++ Assets/RabbitController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RabbitController : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target)
+        {
+            GetComponent<NavMeshAgent>().destination = target.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
